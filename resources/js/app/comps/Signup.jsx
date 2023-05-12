5f46c1ed3fe031329d28b4ca7947b553a1e17ea4
soumiadevv <goualisoumia@gmail.com>
2023-05-12 12:43:24 +0100

    create signup and login

--- resources/js/app/comps/Signup.jsx
+++ resources/js/app/comps/Signup.jsx
@@ -1,8 +1,9 @@
 import React, { useContext, useRef } from 'react'
 import { store } from '/resources/js/app/index';
-import Input from './Input';
 import Login from './Login'
 import { signup } from '/resources/js/api/auth';
+import "/resources/css/login.css";
+import picture from "/resources/assets/signup-splash.jpg";
 
 export default function Signup({ setTarget, triggerRefetch }){
 	const Store = useContext(store);
@@ -29,14 +30,48 @@ export default function Signup({ setTarget, triggerRefetch }){
 			})
 	};
 
-	return <form className="overlay border">
-		<h3>signup form</h3>
-		<Input _ref={username} label="username" placeholder="choose a username"/>
-		<Input _ref={email} label="email" placeholder="enter your email"/>
-		<Input _ref={password} type="password" label="password" placeholder="enter your password"/>
-		<a onClick={swapToLogin}>
-			click here if you don't have an account
-		</a>
-		<Input type="submit" onClick={trySignup}/> 
-	</form>
+	return <div className="form-holder">
+		<div className="image-container">
+		<img src={picture} alt="background" className="background-image" />
+		</div>
+		<form onSubmit={trySignup} className="login-form">
+		<h2 className="login-title">Signup</h2>
+		<div className="input-container">
+			<input
+				type="text"
+				name="username"
+				ref={username}
+				className="login-input"
+				placeholder="username"
+			/>
+		</div>
+		
+		<div className="input-container">
+			<input
+				type="email"
+				name="email"
+				ref={email}
+				className="login-input"
+				placeholder="E-mail"
+			/>
+		</div>
+		<div className="input-container">
+			<input
+				type="password"
+				name="password"
+				ref={password}
+				className="login-input"
+				placeholder="Password"
+			/>
+		</div>
+		<div className="signup-container">
+			<p>
+			you already have an account ?<span className="sip" onClick={swapToLogin }>Login here</span>
+			</p>
+		</div>
+		<button type="submit" className="login-button">
+			Signup
+		</button>
+		</form>
+	</div>
 }
