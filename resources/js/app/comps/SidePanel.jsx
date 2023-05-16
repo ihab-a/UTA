bd3fff67b0ffe26c93c5806650bf94946d729248
soumiadevv <goualisoumia@gmail.com>
2023-05-16 19:36:05 +0100

    some changes in file sidepanel

--- resources/js/app/comps/SidePanel.jsx
+++ resources/js/app/comps/SidePanel.jsx
@@ -1,8 +1,34 @@
 import React from 'react';
 import '/resources/css/side-panel.css';
-
+// import panelprofile from "/resources/assets/panelprofile.jpg";
+import logout from "/resources/assets/icon _logout_.png";
+import edite from "/resources/assets/icon _user edit_.png";
+import logo from "/resources/assets/logo.svg";
+import panelprofile from "/resources/assets/icon _profile circle_.png";
 export default function Player(){
 	return <div id="side-panel" className="border">
-		side panel
+		<div className='panel-header'>
+			<div className='logo-icon'>
+			<img src={logo} alt="background" className="background-image" />
+			</div>
+			<div className='options'>
+				<div className="edit">
+				<img src={edite} alt="background" className="background-image" />
+				</div>
+				
+				<div className="user-img">
+					<img src={panelprofile} alt="background" className="background-image" />
+				</div>
+				
+				<div className="logout">
+				<img src={logout} alt="background" className="background-image" />
+				</div>
+				
+			</div>
+			<div className='user-name'>user name</div>
+		</div>	
+		<div className='panel-playList'>
+			
+		</div>
 	</div>
 }
