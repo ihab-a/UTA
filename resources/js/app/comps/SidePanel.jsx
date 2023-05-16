155754a396d41cd7f8bc5ff67da696075ce05f93
soumiadevv <goualisoumia@gmail.com>
2023-05-16 22:11:10 +0100

    add some new changes in the side-panel

--- resources/js/app/comps/SidePanel.jsx
+++ resources/js/app/comps/SidePanel.jsx
@@ -1,15 +1,19 @@
-import React from 'react';
+
+import React, { useContext } from 'react';
+import { store } from '../index';
 import '/resources/css/side-panel.css';
-// import panelprofile from "/resources/assets/panelprofile.jpg";
+import panelprofilee from "/resources/assets/panelprofile.jpg";
 import logout from "/resources/assets/icon _logout_.png";
 import edite from "/resources/assets/icon _user edit_.png";
 import logo from "/resources/assets/logo.svg";
 import panelprofile from "/resources/assets/icon _profile circle_.png";
 export default function Player(){
+	const Store = useContext(store);
+	const user = Store.user.get();
 	return <div id="side-panel" className="border">
 		<div className='panel-header'>
 			<div className='logo-icon'>
-			<img src={logo} alt="background" className="background-image" />
+				<img src={logo} alt="background" className="background-image" />
 			</div>
 			<div className='options'>
 				<div className="edit">
@@ -25,10 +29,30 @@ export default function Player(){
 				</div>
 				
 			</div>
-			<div className='user-name'>user name</div>
+			<div className='user-name'>{user.username}</div>
+			
 		</div>	
 		<div className='panel-playList'>
-			
-		</div>
-	</div>
+			 <p className='title-playlist'> My playlists</p>
+			 <div>
+			 <div className="playlist-item border">
+				<img src={panelprofile} alt="background" className="avatar" />
+				<p className="playlist-title">my playlist 1</p>
+			</div>
+			<div className="playlist-item border">
+				<img src={panelprofile} alt="background" className="avatar" />
+				<p className="playlist-title">my playlist 2</p>
+			</div>
+			<div className="playlist-item border">
+				<img src={panelprofile} alt="background" className="avatar" />
+				<p className="playlist-title">my playlist 3</p>
+			</div>
+			<div className="playlist-item border">
+				<img src={panelprofile} alt="background" className="avatar" />
+				<p className="playlist-title">my playlist 4</p>
+			</div>
+			 </div>
+	  	</div>
+	 	
+</div>
 }
